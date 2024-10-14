9a8a3af30995d77a8536df0faae2c05e901d538c
Gamakichii <vdlei49@gmail.com>
2024-10-14 11:27:29 +0800

    Troubleshoot for android studio.

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,12 @@ import 'screens/settings_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
