9b5f64e9f19f69248aaf97fec00c1826d632d87a
Hakaii1 <150959650+Hakaii1@users.noreply.github.com>
2024-10-17 23:41:28 +0800

    Avatars

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,85 +1,97 @@
-import 'dart:ui';
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   final String username;
 
   const ProfilePage({Key? key, required this.username}) : super(key: key);
 
-  Future<void> _deleteAccount(BuildContext context) async {
-    final usersCollection = FirebaseFirestore.instance.collection('users');
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
 
-    final confirm = await showDialog<bool>(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(
-          'Delete Account',
-          style: TextStyle(color: Colors.black),
-        ),
-        content: Text(
-          'Are you sure you want to delete your account?',
-          style: TextStyle(color: Colors.black),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context, false), // Cancel
-            child: Text('No', style: TextStyle(color: Colors.black)),
-          ),
-          TextButton(
-            onPressed: () => Navigator.pop(context, true), // Confirm
-            child: Text('Yes', style: TextStyle(color: Colors.black)),
-          ),
-        ],
-      ),
-    );
+class _ProfilePageState extends State<ProfilePage> {
+  String? selectedAvatar;
+  File? _imageFile; // To store the selected image file
 
-    if (confirm == true) {
-      try {
-        final querySnapshot = await usersCollection
-            .where('username', isEqualTo: username)
-            .get();
+  // ImagePicker instance
+  final ImagePicker _picker = ImagePicker();
 
-        if (querySnapshot.docs.isNotEmpty) {
-          await querySnapshot.docs.first.reference.delete();
+  // Method to allow user to pick an image from the gallery
+  Future<void> _pickImageFromGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
 
-          Navigator.pushNamedAndRemoveUntil(
-            context,
-            '/login',
-                (route) => false,
-          );
-        }
-      } catch (e) {
-        print('Error deleting account: $e');
-        _showErrorDialog(context, 'Error', 'Failed to delete the account.');
-      }
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path); // Store the picked image file
+        selectedAvatar = null; // Reset avatar when a custom image is selected
+      });
     }
   }
 
-  void _showErrorDialog(BuildContext context, String title, String message) {
+  // List of available avatar image paths
+  final List<String> avatarPaths = [
+    'assets/avatars/avatar1.png',
+    'assets/avatars/avatar2.png',
+    'assets/avatars/avatar3.png',
+    'assets/avatars/avatar4.png',
+    'assets/avatars/avatar5.png',
+    'assets/avatars/avatar6.png',
+    'assets/avatars/avatar7.png',
+    'assets/avatars/avatar8.png',
+    'assets/avatars/avatar9.png',
+  ];
+
+  // Function to show avatar selection dialog
+  void _showAvatarSelectionDialog() {
     showDialog(
       context: context,
-      builder: (context) => AlertDialog(
-        title: Text(title, style: TextStyle(color: Colors.black)),
-        content: Text(message, style: TextStyle(color: Colors.black)),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text('OK', style: TextStyle(color: Colors.black)),
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: Color(0xFFB7A6E0),
+          title: Text('Select Avatar',style: TextStyle(color: Colors.white),),
+          content: Container(
+            height: 370, // Adjust the height as needed
+            width: double.maxFinite,
+            child: Column(
+              children: [
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3, // Number of avatars per row
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                    ),
+                    itemCount: avatarPaths.length,
+                    itemBuilder: (context, index) {
+                      final avatarPath = avatarPaths[index];
+                      return GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            selectedAvatar = avatarPath; // Set selected avatar
+                            _imageFile = null; // Reset custom image when an avatar is selected
+                          });
+                          Navigator.pop(context); // Close the dialog
+                        },
+                        child: Image.asset(avatarPath),
+                      );
+                    },
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                    _pickImageFromGallery(); // Pick image from gallery
+                  },
+                  child: Text('Upload from Gallery', style: TextStyle(color: Colors.white),),
+                ),
+              ],
+            ),
           ),
-        ],
-      ),
-    );
-  }
-
-  // Logout method
-  void _logout(BuildContext context) {
-    // Clear any user session or authentication state here
-    Navigator.pushNamedAndRemoveUntil(
-      context,
-      '/login',
-          (route) => false, // Remove all routes from the stack
+        );
+      },
     );
   }
 
@@ -112,9 +124,26 @@ class ProfilePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    'Welcome, $username',
+                    'Welcome, ${widget.username}',
                     style: Theme.of(context).textTheme.headlineMedium?.copyWith(
                       color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Center(
+                    child: GestureDetector(
+                      onTap: _showAvatarSelectionDialog, // Open avatar selection or image picker
+                      child: CircleAvatar(
+                        radius: 50,
+                        backgroundImage: _imageFile != null
+                            ? FileImage(_imageFile!) // Display the selected image
+                            : selectedAvatar != null
+                            ? AssetImage(selectedAvatar!) // Display selected avatar
+                            : null,
+                        child: _imageFile == null && selectedAvatar == null
+                            ? Icon(Icons.person, size: 50, color: Colors.white) // Default icon
+                            : null,
+                      ),
                     ),
                   ),
                   SizedBox(height: 20),
@@ -139,7 +168,7 @@ class ProfilePage extends StatelessWidget {
                               Navigator.pushNamed(
                                 context,
                                 '/updatePassword',
-                                arguments: username,
+                                arguments: widget.username,
                               );
                             },
                             child: Padding(
@@ -166,7 +195,7 @@ class ProfilePage extends StatelessWidget {
                                 width: 1,
                               ),
                             ),
-                            onPressed: () => _deleteAccount(context),
+                            onPressed: () => _deleteAccount(context), // Replace with your delete account logic
                             child: Padding(
                               padding: const EdgeInsets.all(12.0),
                               child: Text(
@@ -179,7 +208,6 @@ class ProfilePage extends StatelessWidget {
                             ),
                           ),
                           SizedBox(height: 20),
-                          // Logout button
                           ElevatedButton(
                             style: ElevatedButton.styleFrom(
                               backgroundColor: Colors.transparent,
@@ -192,7 +220,9 @@ class ProfilePage extends StatelessWidget {
                                 width: 1,
                               ),
                             ),
-                            onPressed: () => _logout(context),
+                            onPressed: () {
+                              _logout(context); // Replace with your logout logic
+                            },
                             child: Padding(
                               padding: const EdgeInsets.all(12.0),
                               child: Text(
@@ -215,5 +245,15 @@ class ProfilePage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  // Dummy method for account deletion
+  void _deleteAccount(BuildContext context) {
+    print('Delete account triggered');
+  }
+
+  // Dummy method for logout
+  void _logout(BuildContext context) {
+    print('Logout triggered');
   }
 }
